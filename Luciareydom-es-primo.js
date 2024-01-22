8d8b0a4323512be801cfacda46f20bc1cda3f9dc
luciareydom <lreydom.appventurers@gmail.com>
2024-01-22 10:10:23 +0100

    Tiene en cuenta introducción de números negativos

--- Luciareydom-es-primo.js
+++ Luciareydom-es-primo.js
@@ -1,5 +1,6 @@
     //Crea una función que determine si un número es primo o no
 function esPrimo (numero) {
+    //if (numero <= 1) return false //por números negativos
     for (let i = numero - 1; i > 1; i--) {
         resto = numero % i
         if (resto === 0) {
