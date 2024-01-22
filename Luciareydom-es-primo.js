f11c8aa503f65cf1c20ddff0f9dda84404f27df0
luciareydom <lreydom.appventurers@gmail.com>
2024-01-22 10:02:07 +0100

    Corregida función esPrimo

--- Luciareydom-es-primo.js
+++ Luciareydom-es-primo.js
@@ -1,6 +1,6 @@
     //Crea una función que determine si un número es primo o no
 function esPrimo (numero) {
-    for (i = numero - 1; i > 1; i--) {
+    for (let i = numero - 1; i > 1; i--) {
         resto = numero % i
         if (resto === 0) {
             return false
